4bee6682fa3b5544b3c7372f1b336c49f5786026
frostime <frostime@foxmail.com>
2024-06-30 10:46:08 +0800

    🔧 微调

--- dist/bundle.user.js
+++ dist/bundle.user.js
@@ -2,15 +2,14 @@
 // @name        GPT Input Dialog
 // @description 为一系列 GPT 类网站添加长文输入对话框 | Add a long text input dialog to a series of GPT-like platforms
 // @namespace   gitlab.com/frostime
-// @version     5.6.4
+// @version     5.6.5
 // @match       *://poe.com/chat/*
 // @match       *://poe.com
 // @match       *://chat.mistral.ai/chat
 // @match       *://chat.mistral.ai/chat/*
 // @match       *://chat.openai.com/*
 // @match       *://chatgpt.com/*
-// @match       *://panter.aizex.cn/c/*
-// @match       *://panter.aizex.cn/g/*
+// @match       *://panter.aizex.cn/*
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=chat.openai.com
 // @run-at      document-end
 // @author      frostime
